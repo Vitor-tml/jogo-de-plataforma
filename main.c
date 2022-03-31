2abb3851f2e50749da2b20b3fb1cc80a7b3d1723
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2022-03-30 23:55:56 -0300

    adicionando hibox e f

--- main.c
+++ main.c
@@ -12,13 +12,17 @@ typedef struct {
 } SPRITE;
 
 // Variáveis globais
-int sair = 0;
-SPRITE player = { 0, 0, 0, 0, 68, 90};
-int nTile = 0, direcao = 0;
+SPRITE chao = {0, 0, 0, 0, 60, 60};		// Cenário
+SPRITE player = {0, 0, 0, 0, 68, 90};	// Player
+SPRITE bloco[10][14];					// Matriz do cenário
+int sair = 0;							// Fechar programa
+int nTile = 0, direcao = 0;				// Animação
+int vly = 0, grv = 4, caindo = 1;				// Física do game
 
 // Declaração de Funções
 void sair_allegro();
 void control();
+int hitbox(SPRITE a, SPRITE b);
 
 // Inicio do Programa
 int main()
@@ -34,14 +38,36 @@ int main()
 	// Cria elementos visuais do jogo
 	BITMAP* buffer = create_bitmap(width, height);
 	BITMAP* boneco = load_bitmap("source/steve.bmp", NULL);
+	BITMAP* plataforma = load_bitmap("source/chao.bmp", NULL);
 
 	// Variávei locais
-	int x = 0, y = 0;
+	int i, j;	// Iteração de loops
 	
+	// Alinhamento da matriz (cenário)
+	for(i =  0; i < 10; i ++)
+		for(j = 0; j < 14; j ++)
+		{
+			bloco[i][j].y = 540;
+			bloco[i][j].x = j * 60;
+			bloco[i][j].w = 60;
+			bloco[i][j].h = 60;
+			bloco[i][j].wy = 0;
+			bloco[i][j].wx = 0;
+		}
+
 	// Mantem programa aberto
 	while(!sair)
 	{
 		control();
+		for(i =  0; i < 10; i ++)
+			for(j = 0; j < 14; j ++)
+			{
+				draw_sprite(buffer, plataforma, bloco[i][j].x, bloco[i][j].y);
+				if(hitbox(player, bloco[i][j]))
+				{
+					caindo = 0;
+				}
+			}
 		masked_blit(boneco, buffer, player.wx + nTile * player.w, player.wy + direcao * player.h , player.x, player.y, player.w, player.h);
 		draw_sprite(screen, buffer, 0, 0);
 		rest(50);
@@ -91,8 +117,23 @@ void control()
 		nTile = 0;
 	if(nTile < 0)
 		nTile = 3;
+	
+	if(caindo)
+	{
+		vly += grv;
+		player.y += vly;
+	}
+	caindo = 1;
 }
 
+// Função de Colisão
+int hitbox(SPRITE a, SPRITE b)
+{
+	if((a.x + a.w > b.x) && (a.x < b.x + b.w) && (a.y + a.h > b.y) && (a.y < b.y + b.h))
+		return 1;
+	else 
+		return 0;
+}
 // Função chamada quando o X da janela é clicado
 void sair_allegro()
 {
